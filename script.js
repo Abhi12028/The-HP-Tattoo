f7973ef9461c4aecf3b4087be7b2cc93206438a4
Abhishek Sutar <103806320+Abhi12028@users.noreply.github.com>
2024-06-10 09:47:11 +0530

    Add files via upload
    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSidebar(){
+    const change=document.querySelector('.sidebar');
+    change.style.display='flex';
+}
+function hideSidebar(){
+    const change=document.querySelector('.sidebar');
+    change.style.display='none';
+}
