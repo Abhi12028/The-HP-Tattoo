85ebb622e3e077f509fed766750559cbf5aa0171
Abhishek Sutar <103806320+Abhi12028@users.noreply.github.com>
2024-06-26 11:39:46 +0530

    photos and video optimized
    photos and video are converted to web m format and also made some optimization in video quality

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function showSidebar() {
     sidebar.style.display = 'flex';
     document.body.classList.add('no-scroll');
     isSidebarOpen = true;
-    
+
     // Listen for scroll events
     window.addEventListener('scroll', hideSidebarOnScroll, { passive: true });
 }
@@ -39,4 +39,20 @@ function hideSidebarOnScroll() {
 window.addEventListener('beforeunload', () => {
     window.removeEventListener('scroll', hideSidebarOnScroll);
 });
+
+
+const blurredImageDivs = document.querySelectorAll(".blurred-img"); // Changed variable name to plural
+blurredImageDivs.forEach(div => { // Changed to use div in forEach loop
+    const img = div.querySelector("img"); // Changed to use div to find img
+    function loaded() {
+        div.classList.add("loaded"); // Changed to use div to add class
+    }
+
+    if (img.complete) {
+        loaded();
+    } else {
+        img.addEventListener("load", loaded);
+    }
+});
+
 
